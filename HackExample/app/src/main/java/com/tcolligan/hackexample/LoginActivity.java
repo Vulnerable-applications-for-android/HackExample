38ec9f473d75ba3b003a42147b041e93764ee7d9
Thomas Colligan <tcolligan@apppartner.com>
2016-04-04 20:35:34 -0400

    Added Login UI

--- HackExample/app/src/main/java/com/tcolligan/hackexample/LoginActivity.java
+++ HackExample/app/src/main/java/com/tcolligan/hackexample/LoginActivity.java
@@ -2,12 +2,38 @@ package com.tcolligan.hackexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity
+{
+    // ================================================================================
+    // Class Properties
+    // ================================================================================
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
+    // ================================================================================
+    // Life-Cycle Methods
+    // ================================================================================
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+    }
+
+    // ================================================================================
+    // Button Click Methods
+    // ================================================================================
+
+    public void onLoginButtonClicked(View v)
+    {
+
     }
 }
