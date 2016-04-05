0fdb1a3a5bd77338f8f502ce0ad30e63519cfaeb
Thomas Colligan <tcolligan@apppartner.com>
2016-04-04 22:47:15 -0400

    Added login request execution

--- HackExample/app/src/main/java/com/tcolligan/hackexample/LoginActivity.java
+++ HackExample/app/src/main/java/com/tcolligan/hackexample/LoginActivity.java
@@ -1,9 +1,22 @@
 package com.tcolligan.hackexample;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.tcolligan.hackexample.networking.NetworkingHelper;
+import com.tcolligan.hackexample.networking.PostRequestAsyncTask;
+import com.tcolligan.hackexample.networking.Response;
+import com.tcolligan.hackexample.user.User;
+import com.tcolligan.hackexample.user.UserManager;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity
 {
@@ -26,6 +39,8 @@ public class LoginActivity extends AppCompatActivity
 
         usernameEditText = (EditText) findViewById(R.id.usernameEditText);
         passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        UserManager.getInstance().loadCurrentUser(this);
     }
 
     // ================================================================================
@@ -34,6 +49,72 @@ public class LoginActivity extends AppCompatActivity
 
     public void onLoginButtonClicked(View v)
     {
+        if (!inputIsValid())
+        {
+            Toast.makeText(getApplicationContext(), R.string.invalid_input_toast_text, Toast.LENGTH_SHORT).show();
+            return;
+        }
 
+        if (!Utils.isConnectedToInternet(this))
+        {
+            Toast.makeText(getApplicationContext(), R.string.no_internet_toast_text, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        login(username, password);
+    }
+
+    public void onRegisterButtonClicked(View v)
+    {
+
+    }
+
+    // ================================================================================
+    // Class Instance Methods
+    // ================================================================================
+
+    private boolean inputIsValid()
+    {
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        return !TextUtils.isEmpty(username) && !TextUtils.isEmpty(password);
+    }
+
+    private void login(String username, String password)
+    {
+        String hashedPassword = Utils.md5(password);
+
+        HashMap<String, String> postDataParams = NetworkingHelper.createPostDict();
+
+        postDataParams.put("username", username);
+        postDataParams.put("password", hashedPassword);
+
+        new PostRequestAsyncTask(postDataParams, new PostRequestAsyncTask.PostRequestTaskListener()
+        {
+            @Override
+            public void onResponseReceived(Response response)
+            {
+                Context context = getApplicationContext();
+
+                if (!response.isError())
+                {
+                    JSONObject userData = response.getData();
+                    User loggedInUser = new User(userData);
+                    UserManager.getInstance().saveNewUser(context, loggedInUser);
+                }
+
+                response.displayMessageToastIfNecessary(context);
+            }
+
+            @Override
+            public void onInvalidResponse()
+            {
+                Toast.makeText(getApplicationContext(), R.string.error_toast_text, Toast.LENGTH_SHORT).show();
+            }
+        }).execute(NetworkingHelper.LOGIN);
     }
 }
